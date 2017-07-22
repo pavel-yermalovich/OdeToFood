e8b4b936f64172c8dc658e7e0ccfaa88495deafa
pavel-yermalovich <x48ray@gmail.com>
2017-07-22 15:49:39 +0300

    I

---
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class AboutController
+    {
+        public string Phone() => "+375-29-284-00-00";
+
+        public string Address() => "Belarus";
+    }
+}
---
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Entities;
+
+namespace OdeToFood.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+        Restaurant Add(Restaurant newRestaurant);
+    }
+
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(i => i.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Add(newRestaurant);
+            _context.SaveChanges();
+            return newRestaurant;
+        }
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private static readonly List<Restaurant> _restaurants;
+
+        static InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "The House of Kobe" },
+                new Restaurant { Id = 2, Name = "LJ's and Kat" },
+                new Restaurant { Id = 3, Name = "King's Contrivance" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = _restaurants.Max(r => r.Id) + 1;
+            _restaurants.Add(newRestaurant);
+
+            return newRestaurant;
+        }
+    }
+}
