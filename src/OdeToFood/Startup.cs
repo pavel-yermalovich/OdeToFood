f2699878f727843cd1fd537b17abdbd380f72f81
pavel-yermalovich <x48ray@gmail.com>
2017-08-03 14:00:35 +0300

    Client-side updates

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -67,12 +67,9 @@ namespace OdeToFood
             }
 
             app.UseFileServer();
-
+            app.UseNodeModules(env.ContentRootPath);
             app.UseIdentity();
-
             app.UseMvc(ConfigureRoutes);
-
-            app.Run(ctx => ctx.Response.WriteAsync("Not found"));
         }
 
         private void ConfigureRoutes(IRouteBuilder routeBuilder)
